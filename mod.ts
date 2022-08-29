b2564cfcf52ecd3c9ae83bec330eff617a5ec7b2
61130061 <thezeroyou@gmail.com>
2022-08-29 10:12:57 +0700

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -27,18 +27,6 @@ const symbols = {
    error: '✖'
 }
 
-
-// ANSI Regex Cleaner
-function clearAnsi(text: string) {
-   const  pattern = [
-      '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-      '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
-   ].join('|');
-   
-   const reg = new RegExp(pattern);
-   return text.replace(reg, '');
-}
-
 /* ------ Dora based on Ora ------*/
 class Dora {
    #stream;
